196180d7b5f76033b88b661133f21f1c50f8b5b4
mateusz <mateuszalmannai@googlemail.com>
2017-01-02 14:27:30 +0100

    parse the JSON from the response

--- week2/GiphySearch/javascript/main.js
+++ week2/GiphySearch/javascript/main.js
@@ -11,7 +11,6 @@ document.querySelector(".js-userinput").addEventListener('keyup', function(e){
   // check for 'Enter'
   if (e.which === 13) {
     var input = document.querySelector("input").value;
-    console.log(input);
     pushToDOM(input);
   }
 });
@@ -28,9 +27,9 @@ GiphyAJAXCall.send();
 
 // Event Listener for loading: 
 GiphyAJAXCall.addEventListener('load', function( e ) {
+  // your callback events go here 
   var data = e.target.response;
-  console.log(data);
-// your callback events go here 
+  pushToDOM(data);
 
 });
 
@@ -38,10 +37,19 @@ GiphyAJAXCall.addEventListener('load', function( e ) {
 // 3. Show me the moneys!
 
 function pushToDOM(input){
+  // Deal with API Data, i.e. convert to a JavaScript object
+  var response = JSON.parse(input);
+  var imageUrl = response.data[1].images.fixed_height.url;
+  console.log(imageUrl);
+
   // The following grabs every class on the page, i.e. you grab classes multiple times in contrast to ids
   // Because there can be multiple results, we get an object/array back instead of a single element 
   var container = document.getElementsByClassName("js-container");
-  container[0].innerHTML = "<h1>" + input +"</h1>";
+  // I would alternate with single quotes instead of escaping here
+  container[0].innerHTML = "<img src=\"" + imageUrl + "\" />";
+  // container[0].innerHTML = imageUrl;
+
+
 }
 
 
