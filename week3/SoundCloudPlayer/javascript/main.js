4bd53e6acda98f4928f4800d21a525dfb6a44bb7
mateusz <mateuszalmannai@googlemail.com>
2017-01-05 14:47:34 +0100

    week 3 homework

--- week3/SoundCloudPlayer/javascript/main.js
+++ week3/SoundCloudPlayer/javascript/main.js
@@ -2,19 +2,44 @@
 
 var UI = {};
 
-// UI.EnterPress
+UI.enterPress = function(){
+  document.querySelector(".js-search").addEventListener('keyup', function(e){
+    if (e.which === 13) {
+      var input = document.querySelector("input").value;
+      UI.clearDiv("js-search-results");
+      SoundCloudAPI.getTrack(input);
+    }
+  });
+}
 
-// UI.SubmitClick
+UI.submitClick = function(){
+  document.querySelector(".js-submit").addEventListener('click', function(){
+    var input = document.querySelector("input").value;
+    UI.clearDiv("js-search-results");
+    SoundCloudAPI.getTrack(input);
+  });
+}
 
-// add clear button to reset playlist: Storage.clear()
+UI.clearDiv = function(divClass){
+  document.getElementsByClassName(divClass)[0].innerHTML="";
+  if (divClass === "js-search-results") {
+    document.getElementsByClassName(divClass)[0].style.backgroundImage = "url(http://lorempixel.com/200/200/abstract/)";
+  }
+}
 
+UI.clearStorage = function(){
+  document.querySelector(".js-go").addEventListener('click', function(){
+    UI.clearDiv("js-playlist");
+    localStorage.clear();
+  });
+}
 // skin it differently 
 
 
 
 /* 2. Query Soundcloud API */
 
-// Sometimes when people make objects that wrap large pieces of code and use the like Classes
+// Sometimes when people make objects that wrap large pieces of code and use them like Classes
 // they tend to make the first letter of the name uppercase
 var SoundCloudAPI = {};
 
@@ -30,19 +55,17 @@ SoundCloudAPI.getTrack = function(inputValue) {
   SC.get('/tracks', {
     q: inputValue
   }).then(function(tracks) {
-    console.log(tracks);
     // Only render tracks if we have tracks
     SoundCloudAPI.renderTracks(tracks);
   });
 }
 
+
+/* 3. Display the cards */
 SoundCloudAPI.renderTracks = function(tracks) {
-  /* 3. Display the cards */
 
   // pass in an event 
   tracks.forEach(function(track) {
-
-    // console.log(track);
 
     // create div
     var card = document.createElement('div');
@@ -94,32 +117,30 @@ SoundCloudAPI.renderTracks = function(tracks) {
 
 }
 
-
-SoundCloudAPI.init();
-SoundCloudAPI.getTrack("Stivs");
-
 /* 4. Add to playlist and play */
-
 SoundCloudAPI.getEmbed = function(trackUrl) {
   // console.log('Clicked; I\'m in getEmbed');
   SC.oEmbed(trackUrl, {
     auto_play: true
   }).then(function(embed) {
-    // console.log('oEmbed response: ', embed);
 
     var sideBar = document.querySelector('.js-playlist');
-
     var box = document.createElement('div');
     box.innerHTML = embed.html;
 
     // save songs in localStorage to survive page refreshes
     sideBar.insertBefore(box, sideBar.firstChild);
     localStorage.setItem("key", sideBar.innerHTML);
-    // console.log(sideBar.innerHTML);
   });
 }
+
+// Initialize API with key
+SoundCloudAPI.init();
+// Register Listeners
+UI.enterPress();
+UI.submitClick();
+UI.clearStorage();
 
 // put things back in the sidebar, i.e. populate on load
 var sideBar = document.querySelector(".js-playlist");
 sideBar.innerHTML = localStorage.getItem("key");
-
