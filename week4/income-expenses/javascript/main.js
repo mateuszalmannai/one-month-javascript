0fde228113b42e8f0c85f5d902b0b7ffbf3a1da2
mateusz <mateuszalmannai@googlemail.com>
2017-01-07 12:41:17 +0100

    finish homework week 4

--- week4/income-expenses/javascript/main.js
+++ week4/income-expenses/javascript/main.js
@@ -6,6 +6,7 @@ GoogleChartsAPI.init = function() {
 
 GoogleChartsAPI.render = function(sheetData, tabletop) {
 
+  UI.render(sheetData);
   var chartData = [];
   sheetData.forEach(function(sheetData) {
     chartData.push(new Array(sheetData.City, parseInt(sheetData.Difference), parseInt(sheetData.Income)));
@@ -44,17 +45,38 @@ TabletopAPI.init = function(url) {
 var UI = {};
 
 UI.render = function(data) {
-  data.forEach(function(data) {
-    var section = document.createElement('div');
-    section.classList.add('section');
-    section.innerHTML = '<h1>' + data.Category + '</h1>';
-    var searchResults = document.querySelector('.js-search-results');
-    // searchResults.appendChild(section);
-  });
-  window.addEventListener('DOMContentLoaded', init)
+  console.log("In render: " + data);
+  var heading = document.createElement('div');
+  heading.classList.add('header');
+  heading.innerHTML = '<h1>' + data[0].Information + '</h1>';
+
+  var info = document.createElement('div');
+  info.classList.add('info');
+  info.innerHTML = data[1].Information;
+
+  var more_info = document.createElement('div');
+  more_info.classList.add('more_info');
+  more_info.innerHTML = data[2].Information;
+
+  var conclusion = document.createElement('div');
+  conclusion.classList.add('conclusion');
+  conclusion.innerHTML = data[3].Information;
+
+  var disclaimer = document.createElement('div');
+  disclaimer.classList.add('disclaimer');
+  disclaimer.innerHTML = data[4].Information;
+
+  var searchResults = document.querySelector('.js-search-results');
+  searchResults.appendChild(heading);
+  searchResults.appendChild(info);
+  searchResults.appendChild(more_info);
+  searchResults.appendChild(conclusion);
+  searchResults.appendChild(disclaimer);
 }
 
 
 GoogleChartsAPI.init();
 TabletopAPI.init('https://docs.google.com/spreadsheets/d/1wyg34DtndqrWYlbSD-3S9Tw5YxgOWQtcv2p2JdS6L2Q/pubhtml');
+// window.addEventListener('DOMContentLoaded', TabletopAPI.init);
 // Data Source: https://www.vexcash.com/blog/lebenshaltungskosten-deutschland-2016/
+// Try this: https://www.laenderdaten.info/lebenshaltungskosten.php
